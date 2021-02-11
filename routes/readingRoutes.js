4b5eac3e8b257beed4dd32c5ed7ab6e42b4bdaab
normal-crayon <prithviraj.yuvaraj@gmail.com>
2021-02-11 09:07:13 +0530

    post working now, able to add to db

--- routes/readingRoutes.js
+++ routes/readingRoutes.js
@@ -7,22 +7,18 @@ const read = require("../models/Reading.js")
 //          - Add a reading to the database
 router.post('/add', (req, res) => {
     console.log("Body : ", req.body);
-    var value = new read({ value : 10 });
-    read.insertMany([
-        {value : value}
-    ],
-    function(err, result){
-        if (err){
-            res.send(err);
-        }
-        else{
-            res.send(result);
-        }
+    var value = new read({ value : req.body });
+    value.save().then( result => {
+        console.log("value added !");
+        
+    })
+    .catch( err => {
+        console.log(err)
     })
     return res.json({
         message : "Received",
         value : value
-    });
+    })
 }) 
 
 router.get('/fetch',(req, res)=>{
