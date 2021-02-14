8f2193c1a3fda6b2942ab32321ba2a27d2e61b3b
normal-crayon <prithviraj.yuvaraj@gmail.com>
2021-02-14 15:55:35 +0530

    added 'n' parameter

--- routes/readingRoutes.js
+++ routes/readingRoutes.js
@@ -9,19 +9,24 @@ router.post('/add', (req, res) => {
     console.log("Body : ", req.body);
     var value = new read({ value : req.body });
     value.save().then( result => {
-        console.log("value added !");
+        //console.log("value added !");
+        return res.json({
+            message : "insertion success !",
+            value : value
+        })
         
     })
     .catch( err => {
-        console.log(err)
-    })
-    return res.json({
-        value : value
+        //console.log(err)
+        return res.json({
+            message : 'insertion failed',
+            error : err
+        })
     })
 }) 
 
-router.get('/fetch',(req, res)=>{
-    const values = read.find().then( result => {
+router.get('/fetch',(req, res, n)=>{
+    const values = read.find().sort({_id : 1}).limit(n).then( result => {
         return res.json(result);
     });
 })
